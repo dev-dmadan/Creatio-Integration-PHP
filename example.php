0c055256a28ec08d5c0dfa0fdc9f238911d92b28
Romadan Saputra <42397721+dev-dmadan@users.noreply.github.com>
2020-03-18 13:35:14 +0700

    Create example.php
    Example how to use library

---
+++ example.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__. '/vendor/autoload.php';
+require_once 'CreatioIntegrationPHP.php';
+
+// setup url, username, and password to access creatio
+$config = array(
+    'url' => 'http://localhost:8082',
+    'username' => 'Supervisor',
+    'password' => 'Supervisor'
+);
+
+// init library
+$creatio = new CreatioIntegration($config['url'], $config['username'], $config['password']);
+
+// make RESTFull request
+$request = $creatio->rest('POST', [
+    'service' => 'PeriodicalEditionWebService',
+    'method' => 'GetTotalPlannedIssues'
+], array(
+    'Code' => '123'
+), false);
+
+// doing somthing or anything with the response
+echo '<pre>';
+var_dump($request);
+echo '</pre>';
